383c3fb6bd72b73505c7a148240b670f70843c26
cgkw94 <colingohh@gmail.com>
2021-11-22 11:31:36 +0800

    v1

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
 "use strict";
 
-//create 25 rows
+//create 20 rows
 for (let i = 20; i > 0; i--) {
   let row = document.createElement("div");
   row.setAttribute("class", "all-rows sliding");
   row.setAttribute("id", `row${i}`);
   document.querySelector(".game-container").append(row);
 }
+
+const stopRow1 = () => {
+  //select row
+  let currentRow = document.querySelector("#row1");
+
+  //retrieve CSS left position
+  let currentLeftPosition = window
+    .getComputedStyle(currentRow)
+    .getPropertyValue("left");
+  //   console.log(currentLeftPosition);
+
+  //remove sliding animation
+  currentRow.classList.remove("sliding");
+
+  //assign left position it to current row CSS
+  currentRow.style.left = currentLeftPosition;
+};
+
+const stopRow2 = () => {
+  //select row
+  let currentRow = document.querySelector("#row2");
+
+  //retrieve CSS left position
+  let currentLeftPosition = window
+    .getComputedStyle(currentRow)
+    .getPropertyValue("left");
+  //   console.log(currentLeftPosition);
+
+  //remove sliding animation
+  currentRow.classList.remove("sliding");
+
+  //assign left position it to current row CSS
+  currentRow.style.left = currentLeftPosition;
+};
+
+document.querySelector("#stop-btn").addEventListener("click", stopRow1);
+document.querySelector("#stop-btn2").addEventListener("click", stopRow2);
